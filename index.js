2586ed1946838d58c2fbd37e0e1971aaaa053196
Ayan-dip <ayandip6969@gmail.com>
2026-04-23 20:10:55 +0530

    feat: implement persistent board state synchronization across connected clients

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ app.get('/style.css', (req, res) => {
      res.sendFile(join(import.meta.dirname, 'style.css'));
 })
 
+// This object stays in the server's memory 24/7!
+const boardState = {};
+
 io.on('connect', (socket) => {
      console.log('New User connected at port number:', socket.id);
+     
+     // 1. Immediately send the new user the CURRENT state of the board
+     socket.emit('initial-state', boardState);
+
      socket.on('color-update', (data) => {
+          // 2. Remember this change in the server's memory
+          boardState[data.id] = data.color;
+          
+          // 3. Broadcast the change to everyone else
           socket.broadcast.emit("change-is-here", data);
      })
 })
